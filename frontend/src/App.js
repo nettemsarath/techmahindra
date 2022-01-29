c41941059eef359a046cc741fa4d68ff035bb879
sarath <sarath@avantari.org>
2022-01-30 00:04:49 +0530

    doc added yml file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 import "./App.css";
 
 const App = () => {
-  return <div className='App'></div>;
+  return <div className='App'> NETTEM SARATH </div>;
 };
 
 export default App;
